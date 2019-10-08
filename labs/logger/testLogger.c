8c87ae1c2260e863c001b8f0f5ab1453185fcab6
Alexis Mendoza V <Alexis_valenciia@hotmail.com>
2019-10-08 15:35:37 -0500

    Logger

--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,9 @@
 
 
 int main() {
+	infof("Hola: %s, int: %d, char: %c\n", "Mundo!", 25, 'k');
+	warnf("Hola: %s, int: %d, char: %c\n", "Mundo!", 25, 'k');
+	errorf("Hola: %s, int: %d, char: %c\n", "Mundo!", 25, 'k');
+	panicf("Hey");
     return 0;
 }

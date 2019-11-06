5d22fa4e7c783a80725c609ec64fb6f58dac0add
Alexis Mendoza V <Alexis_valenciia@hotmail.com>
2019-11-06 11:05:46 -0600

    strlib_finished

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main() {
+	char *name = "Alexis";
+	printf("%d\n", mystrlen(name));
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,37 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 
 int mystrlen(char *str){
-    return 0;
+	int len = 0;
+	while(*str != NULL){
+		len++;
+		str++;
+	}
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+	int newLenght = mystrlen(origin) + mystrlen(addition);
+	char *newString = malloc(sizeof(char)*newLenght);
+	for(int i = 0; i < mystrlen(origin); i++){
+		newString[i] = origin[i];
+	}
+	for(int j = 0; j < mystrlen(addition); j++){
+		newString[j+mystrlen(origin)] = addition[j];
+	}
+    return newString;
 }
 
 int mystrfind(char *origin, char *substr){
+    for (int i = 0; i < mystrlen(origin); ++i)
+    {
+    	for(int j = 0; j < mystrlen(substr); j++){
+    		if(origin[i+j] != substr[j]){
+    			break;
+    		}else if(j == mystrlen(substr)-1){
+    			return 1;
+    		}
+    	}
+    }
     return 0;
 }

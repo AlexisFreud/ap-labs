ce7111ae6f53795eb3704525dbbe288c987aff84
Alexis Mendoza V <Alexis_valenciia@hotmail.com>
2019-11-22 19:25:35 -0600

    add something in hlab5

--- labs/lab5.1/hello_par.c
+++ labs/lab5.1/hello_par.c
@@ -1,4 +1,15 @@
+#include "omp.h"
+#include <stdio.h>
 
 int main() {
+
+	#pragma omp parallel
+	{
+		int ID = omp_get_thread_num();
+		for(int i = 1+(100*ID); i<100*(ID+1); i++){
+			printf("Im %d and going %d\n", ID, i);
+		}
+	}
+
     return 0;
 }
